d37dd1916b3b1510c244698c0a0c73f2b8838f12
Fengnaaaais <tajibay.baglan@gmail.com>
2025-08-28 14:19:40 +0500

    Include api router

--- src/main.py
+++ src/main.py
@@ -3,17 +3,13 @@ from uvicorn import run
 from fastapi import FastAPI
 
 from core.utils.case import to_snake
+from api import router as api_router
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {
-        "ok": True,
-    }
-
+app.include_router(api_router, prefix="/api")
 
 if __name__ == "__main__":
     run("main:app", reload=True)
