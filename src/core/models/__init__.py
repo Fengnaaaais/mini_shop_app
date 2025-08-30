80fd2f7cd197385f6dcfbf591522f61b870e1389
Fengnaaaais <tajibay.baglan@gmail.com>
2025-08-30 12:04:30 +0500

    fix: add AccessToken import to __init__.py

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,11 +1,8 @@
-__all__ = (
-    "Base",
-    "Category",
-    "db_helper",
-    "Product",
-)
+__all__ = ("Base", "Category", "db_helper", "Product", "User", "AccessToken")
 
 from .base import Base
 from .category import Category
 from .db_helper import db_helper
 from .product import Product
+from .user import User
+from .access_token import AccessToken
