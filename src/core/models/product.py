41dc1e0a02ac0dad3fe48e4c1c20db907a4f1377
Fengnaaaais <tajibay.baglan@gmail.com>
2025-08-30 17:15:32 +0500

    Add: ForeignKey Product <-> User

--- src/core/models/product.py
+++ src/core/models/product.py
@@ -4,11 +4,14 @@ from sqlalchemy.orm import Mapped, relationship, mapped_column
 from sqlalchemy import Integer, ForeignKey
 
 from core.mixins import IDColumnMixin
+from core.types import user_id_type
+
 from .base import Base
 
 
 if TYPE_CHECKING:
     from .category import Category
+    from .user import User
 
 
 class Product(Base, IDColumnMixin):
@@ -20,3 +23,6 @@ class Product(Base, IDColumnMixin):
 
     price: Mapped[int]
     image: Mapped[str]
+
+    user_id: Mapped[user_id_type] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="products")
--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, relationship
 
 from core.types import user_id_type
 from core.mixins import IDColumnMixin
@@ -10,9 +11,12 @@ from .base import Base
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from .product import Product
 
 
 class User(Base, IDColumnMixin, SQLAlchemyBaseUserTable[user_id_type]):
+    products: Mapped[list["Product"]] = relationship("Product", back_populates="user")
+
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
